832c4455725a1b3f2518edd5d077fb5b341687bd
duck3n <duck3n@DESKTOP-LS0DF6L>
2020-06-28 23:12:18 +0900

    20/06/28 - 게시글 작성 sql, controller 생성, root-context 설정

---
+++ duck3nBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -0,0 +1,25 @@
+package kr.co.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.BoardVO;
+
+// @Repository : 일반적으로 DAO에 쓰이며, DB Exception -> DataAccessException으로 변환
+// @Inject : 특정 framework에 종속되지 않은 어플리케이션을 구성하기 위한 주입
+
+@Repository
+public class BoardDAOImpl implements BoardDAO{
+	
+	@Inject
+	private SqlSession ss;
+	
+	@Override
+	public void write(BoardVO boardVO) throws Exception {
+		ss.insert("boardMapper.insert", boardVO);
+		
+	}
+
+}
---
+++ duck3nBoard/src/main/java/kr/co/service/BoardServiceImpl.java
@@ -0,0 +1,26 @@
+package kr.co.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.dao.BoardDAO;
+import kr.co.vo.BoardVO;
+
+// @Service : 비즈니스 로직이 들어가는 곳에 사용하는 어노테이션 
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+	@Inject
+	private BoardDAO dao;
+	
+	// 게시글 작성
+	@Override
+	public void write(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		dao.write(boardVO);
+		
+	}
+
+}
